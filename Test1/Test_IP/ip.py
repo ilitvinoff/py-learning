821e37b5dbfb7d881ca293f22aaca0fa85bbdf91
ilitvinov <ihorlitvinoff@gmail.com>
2021-02-24 15:45:24 +0200

    still learning py and expirementing with git

---
+++ Test1/Test_IP/ip.py
@@ -0,0 +1,2 @@
+def get_local_host():
+    return '127.0.0.1'
--- Test1/main.py
+++ Test1/main.py
@@ -1,16 +1,5 @@
-    # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+from Test_IP import ip
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(ip.get_local_host())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
